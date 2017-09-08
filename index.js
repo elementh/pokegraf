ad5fcee53fbe2e1d67a0bcf3fdf685924932d0b1
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2017-09-08 15:59:30 +0200

    Testing

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 'use strict'
-
 require('dotenv-safe').load()
+
+const Telegraf = require('telegraf')
+const { Extra, Markup } = require('telegraf')
+
+const pokegraf = new Telegraf(process.env.BOT_TOKEN)
+
+pokegraf.command('start', ({ from, reply }) => {
+  console.log('start', from)
+  return reply(`Welcome ${from.first_name} to Pokegraf.\n\nPlease choose your language:`, Markup
+    .keyboard([['🇪🇸 Spanish'], ['🇬🇧 English']])
+    .oneTime()
+    .resize()
+    .extra()
+  )
+})
+
+pokegraf.startPolling()
