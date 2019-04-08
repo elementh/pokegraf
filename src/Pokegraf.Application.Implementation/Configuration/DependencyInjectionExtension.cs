0bf5bb52ecf0944913e5cfd717419c5c878876b5
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-08 17:32:03 +0200

    Added DI for Application

---
+++ src/Pokegraf.Application.Implementation/Configuration/DependencyInjectionExtension.cs
@@ -0,0 +1,44 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Pokegraf.Application.Contract.Client;
+using Pokegraf.Application.Contract.Service;
+using Pokegraf.Application.Implementation.Client;
+using Pokegraf.Application.Implementation.Service;
+using Pokegraf.Application.Implementation.Service.Background;
+using Scrutor;
+
+namespace Pokegraf.Application.Implementation.Configuration
+{
+    public static class DependencyInjectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddBackgroundServices(configuration);
+            services.AddServices(configuration);
+
+            return services;
+        }
+        
+        private static IServiceCollection AddBackgroundServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IBotClient, BotClient>();
+            services.AddScoped<ITelegramService, TelegramService>();
+            services.AddSingleton<IHostedService, TelegramBackgroundService>();
+
+            return services;
+        }
+                
+        private static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Scan(scan => scan
+                .FromAssemblyOf<TelegramService>()
+                .AddClasses(classes => classes.Where(c => c.Name.EndsWith("Service") && c.Name != "TelegramService"))
+                .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+
+            return services;
+        }
+    }
+}
