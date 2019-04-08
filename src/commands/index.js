006e8bf366be7b59f325e47a80f5bef8ef10e9e7
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-08 13:13:13 +0200

    Deleted old JS

--- src/commands/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  start: require('./start'),
-  about: require('./about'),
-  random: require('./random'),
-  fusion: require('./fusion'),
-  help: require('./help'),
-  pokemon: require('./pokemon')
-}
