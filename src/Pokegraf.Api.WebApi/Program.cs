16ee2d986edc846c25164a991be0006a9fbbca24
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-08 17:20:46 +0200

    Updated program and startup to use IoC

--- src/Pokegraf.Api.WebApi/Program.cs
+++ src/Pokegraf.Api.WebApi/Program.cs
@@ -1,24 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Pokegraf.Common.IoC.Configuration;
+using Serilog;
 
 namespace Pokegraf.Api.WebApi
 {
     public class Program
     {
+        private static IConfiguration Configuration { get; } = ConfigurationExtension.LoadConfiguration();
+
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseSerilog();
+        
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+            Log.Logger = ConfigurationExtension.LoadLogger(Configuration);
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            try
+            {
+                Log.Information("Starting Pokegraf");
+
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Pokegraf terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
     }
 }
--- src/Pokegraf.Api.WebApi/Startup.cs
+++ src/Pokegraf.Api.WebApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Pokegraf.Common.IoC.Configuration;
 
 namespace Pokegraf.Api.WebApi
 {
@@ -26,6 +27,8 @@ namespace Pokegraf.Api.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDependencyInjection(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
