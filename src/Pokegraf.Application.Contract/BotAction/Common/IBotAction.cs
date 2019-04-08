d1f24896e61436f48c7b092ec9857c8354a1fe81
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-08 16:20:02 +0200

    Updated BotAction interface and implemented it

--- src/Pokegraf.Application.Contract/BotAction/Common/IBotAction.cs
+++ src/Pokegraf.Application.Contract/BotAction/Common/IBotAction.cs
@@ -1,10 +1,14 @@
+using MediatR;
 using Pokegraf.Common.Result;
-using Telegram.Bot.Requests.Abstractions;
+using Telegram.Bot.Types;
 
 namespace Pokegraf.Application.Contract.BotAction.Common
 {
     public interface IBotAction : IRequest<Result>
     {
-        
+        int MessageId { get; set; }
+        Chat Chat { get; set; }
+        User From { get; set; }
+        string Text { get; set; }
     }
 }
---
+++ src/Pokegraf.Application.Implementation/BotAction/Common/BotAction.cs
@@ -0,0 +1,13 @@
+using Pokegraf.Application.Contract.BotAction.Common;
+using Telegram.Bot.Types;
+
+namespace Pokegraf.Application.Implementation.BotAction.Common
+{
+    public class BotAction : IBotAction
+    {
+        public int MessageId { get; set; }
+        public Chat Chat { get; set; }
+        public User From { get; set; }
+        public string Text { get; set; }
+    }
+}
