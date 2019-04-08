d25afb8667de7aca16c7c5291c674d3fd9565b19
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-08 23:00:42 +0200

    Started to implement BotActionFactory

---
+++ src/Pokegraf.Application.Implementation/BotActions/Common/BotActionFactory.cs
@@ -0,0 +1,51 @@
+using System.Linq;
+using Pokegraf.Application.Contract.BotAction.Common;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Pokegraf.Application.Implementation.BotActions.Common
+{
+    public class BotActionFactory : IBotActionFactory
+    {
+        public IBotAction GetBotAction(Message message)
+        {
+            var command = GetCommand(message);
+
+            var botAction = ToBotAction(message);
+            switch (command)
+            {
+                case "/pokemon":
+                case "pkm":
+                    return botAction as PokemonCommandAction;
+                default:
+                    return botAction;
+            }
+        }
+        
+        private string GetCommand(Message message)
+        {
+            if (message.Entities.First()?.Type != MessageEntityType.BotCommand) return null;
+            
+            var command = message.EntityValues.First();
+
+            if (!command.Contains('@')) return command;
+                
+            if (!command.ToLower().Contains("@pokegraf")) return null;
+
+            command = command.Substring(0, command.IndexOf('@'));
+
+            return command;
+        }
+        
+        private BotAction ToBotAction(Message message)
+        {
+            return new BotAction()
+            {
+                MessageId = message.MessageId,
+                Chat = message.Chat,
+                From = message.From,
+                Text = message.Text
+            };
+        }
+    }
+}
