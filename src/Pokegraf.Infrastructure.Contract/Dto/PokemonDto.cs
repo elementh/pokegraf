7324bfd50b1a89b3c8b1ef790d234e8cc8560c6e
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-09 15:44:29 +0200

    Updated PokemonDto, added Name

--- src/Pokegraf.Infrastructure.Contract/Dto/PokemonDto.cs
+++ src/Pokegraf.Infrastructure.Contract/Dto/PokemonDto.cs
@@ -5,6 +5,7 @@ namespace Pokegraf.Infrastructure.Contract.Dto
     public class PokemonDto
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public string Description { get; set; }
         public Uri Image { get; set; }
         public Tuple<int, string> Before { get; set; }
