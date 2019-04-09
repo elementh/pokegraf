24f97ca85c6b3aa79b45525e61f98a994e876b5e
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-09 22:49:44 +0200

    Changed Notification to request in IResponse

---
+++ src/Pokegraf.Application.Implementation/BotActions/Responses/TextResponse.cs
@@ -0,0 +1,18 @@
+using Pokegraf.Application.Contract.BotActions.Responses;
+using Pokegraf.Common.Request;
+using Pokegraf.Common.Result;
+
+namespace Pokegraf.Application.Implementation.BotActions.Responses
+{
+    public class TextResponse : Request<Result>, IResponse
+    {
+        public long ChatId { get; set; }
+        public string Text { get; set; }
+
+        public TextResponse(long chatId, string text)
+        {
+            ChatId = chatId;
+            Text = text;
+        }
+    }
+}
