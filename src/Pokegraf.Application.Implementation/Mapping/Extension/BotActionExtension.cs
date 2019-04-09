1a561f5dcb5936ce8ac29ed4fa2075608e8cf760
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-09 14:36:29 +0200

    Added BotAction mapping to other actions

---
+++ src/Pokegraf.Application.Implementation/Mapping/Extension/BotActionExtension.cs
@@ -0,0 +1,31 @@
+using AutoMapper;
+using Pokegraf.Application.Implementation.BotActions.Commands.Fusion;
+using Pokegraf.Application.Implementation.BotActions.Commands.Pokemon;
+using Pokegraf.Application.Implementation.BotActions.Common;
+using Pokegraf.Common.Result;
+
+namespace Pokegraf.Application.Implementation.Mapping.Extension
+{
+    public static class BotActionExtension
+    {
+        internal static IMapper Mapper { get; set; }
+
+        static BotActionExtension()
+        {
+            Mapper = new MapperConfiguration(cfg => { }).CreateMapper();
+        }
+
+        public static PokemonCommandAction ToPokemonCommandAction(this BotAction botAction)
+        {
+            return botAction != null
+                ? Mapper.Map<PokemonCommandAction>(botAction)
+                : null;
+        }
+        public static FusionCommandAction ToFusionCommandAction(this BotAction botAction)
+        {
+            return botAction != null
+                ? Mapper.Map<FusionCommandAction>(botAction)
+                : null;
+        }
+    }
+}
---
+++ src/Pokegraf.Application.Implementation/Mapping/Profile/BotActionProfile.cs
@@ -0,0 +1,15 @@
+using Pokegraf.Application.Implementation.BotActions.Commands.Fusion;
+using Pokegraf.Application.Implementation.BotActions.Commands.Pokemon;
+using Pokegraf.Application.Implementation.BotActions.Common;
+
+namespace Pokegraf.Application.Implementation.Mapping.Profile
+{
+    public class BotActionProfile : AutoMapper.Profile
+    {
+        public BotActionProfile()
+        {
+            CreateMap<BotAction, PokemonCommandAction>();
+            CreateMap<BotAction, FusionCommandAction>();
+        }
+    }
+}
