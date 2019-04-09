c86f49e7a53e67fd811dcb834cd22112403c30f1
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-09 12:17:54 +0200

    Corrected BotAction

--- src/Pokegraf.Application.Implementation/BotActions/Common/BotAction.cs
+++ src/Pokegraf.Application.Implementation/BotActions/Common/BotAction.cs
@@ -1,9 +1,11 @@
 using Pokegraf.Application.Contract.BotAction.Common;
+using Pokegraf.Common.Request;
+using Pokegraf.Common.Result;
 using Telegram.Bot.Types;
 
 namespace Pokegraf.Application.Implementation.BotActions.Common
 {
-    public class BotAction : IBotAction
+    public class BotAction : Request<Result>, IBotAction
     {
         public int MessageId { get; set; }
         public Chat Chat { get; set; }
