8efa2c7b87e08db947f8959e4b2551435159a37e
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-11 12:24:09 +0200

    Created and implemented photo with keyboard response

---
+++ src/Pokegraf.Application.Implementation/BotActions/Responses/PhotoWithKeyboard/PhotoWithKeyboardResponseHandler.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Pokegraf.Application.Contract.Client;
+using Pokegraf.Common.Result;
+
+namespace Pokegraf.Application.Implementation.BotActions.Responses.PhotoWithKeyboard
+{
+    public class PhotoWithKeyboardResponseHandler : Pokegraf.Common.Request.RequestHandler<PhotoWithKeyboardResponse, Result>
+    {
+        protected readonly IBotClient Bot;
+
+        public PhotoWithKeyboardResponseHandler(ILogger<Pokegraf.Common.Request.RequestHandler<PhotoWithKeyboardResponse, Result>> logger,
+            IMediator mediatR, IBotClient bot) : base(logger, mediatR)
+        {
+            Bot = bot;
+        }
+
+        public override async Task<Result> Handle(PhotoWithKeyboardResponse request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await Bot.Client.SendPhotoAsync(request.ChatId, request.Photo, replyMarkup: request.Keyboard,
+                    cancellationToken: cancellationToken);
+
+                return Result.Success();
+            }
+            catch (Exception e)
+            {
+                Logger.LogError("Unhandled error sending photo response with inline keyboard", e);
+
+                return Result.UnknownError(new List<string> {e.Message});
+            }
+        }
+    }
+}
