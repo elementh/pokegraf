264fd868f8b25ca020b6758e7559ea35bee4b75f
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-16 12:56:39 +0200

    Refactored TelegramService to use scope per event

--- src/Pokegraf.Application.Implementation/Service/TelegramService.cs
+++ src/Pokegraf.Application.Implementation/Service/TelegramService.cs
@@ -2,12 +2,16 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Pokegraf.Application.Contract.BotActions.Common;
-using Pokegraf.Application.Contract.Client;
+using Pokegraf.Application.Contract.Common.Actions;
+using Pokegraf.Application.Contract.Common.Client;
+using Pokegraf.Application.Contract.Common.Context;
 using Pokegraf.Application.Contract.Service;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
 
 namespace Pokegraf.Application.Implementation.Service
 {
@@ -16,14 +20,14 @@ namespace Pokegraf.Application.Implementation.Service
         protected readonly ILogger<TelegramService> Logger;
         protected readonly IBotClient Bot;
         protected readonly IMediator MediatR;
-        protected readonly IBotActionFactory BotActionFactory;
+        protected readonly IServiceScopeFactory ServiceScopeFactory;
 
-        public TelegramService(ILogger<TelegramService> logger, IBotClient bot, IMediator mediatR, IBotActionFactory botActionFactory)
+        public TelegramService(ILogger<TelegramService> logger, IBotClient bot, IMediator mediatR, IServiceScopeFactory serviceScopeFactory)
         {
             Logger = logger;
             Bot = bot;
             MediatR = mediatR;
-            BotActionFactory = botActionFactory;
+            ServiceScopeFactory = serviceScopeFactory;
         }
 
         public void StartPokegrafBot()
@@ -39,75 +43,99 @@ namespace Pokegraf.Application.Implementation.Service
 
         private async void HandleOnMessage(object sender, MessageEventArgs e)
         {
-            try
+            using (var scope = ServiceScopeFactory.CreateScope())
             {
-                var botActionResult = BotActionFactory.GetBotAction(e.Message);
+                var botContext = scope.ServiceProvider.GetRequiredService<IBotContext>();
+                botContext.Populate(e.Message);
 
-                if (!botActionResult.Succeeded) return;
+                var actionSelector = scope.ServiceProvider.GetRequiredService<IBotActionSelector>();
 
-                await Bot.Client.SendChatActionAsync(e.Message.Chat.Id, ChatAction.Typing);
-
-                var requestResult = await MediatR.Send(botActionResult.Value);
-
-                if (!requestResult.Succeeded && !requestResult.Errors.ContainsKey("not_found"))
+                try
                 {
-                    Logger.LogError("{BotAction} was not processed correctly: {@Errors}",
-                        botActionResult.Value.GetType().Name, requestResult.Errors);
+                    var botActionResult = actionSelector.GetCommandAction();
+
+                    if (!botActionResult.Succeeded) return;
+
+                    await Bot.Client.SendChatActionAsync(e.Message.Chat.Id, ChatAction.Typing);
+
+                    var requestResult = await MediatR.Send(botActionResult.Value);
+
+                    if (!requestResult.Succeeded && !requestResult.Errors.ContainsKey("not_found"))
+                    {
+                        Logger.LogError("{BotAction} was not processed correctly: {@Errors}",
+                            botActionResult.Value.GetType().Name, requestResult.Errors);
+                    }
                 }
-            }
-            catch (Exception exception)
-            {
-                Logger.LogError(exception, "Unhandled error processing message ({@Message}).", e.Message);
+                catch (Exception exception)
+                {
+                    Logger.LogError(exception, "Unhandled error processing message ({@Message}).", e.Message);
+                }
             }
         }
 
         private async void HandleOnCallbackQuery(object sender, CallbackQueryEventArgs e)
         {
-            try
+            using (var scope = ServiceScopeFactory.CreateScope())
             {
-                var callbackQueryResult = BotActionFactory.GetCallbackAction(e.CallbackQuery);
+                var botContext = scope.ServiceProvider.GetRequiredService<IBotContext>();
+                botContext.Populate(e.CallbackQuery);
 
-                if (!callbackQueryResult.Succeeded) return;
+                var actionSelector = scope.ServiceProvider.GetRequiredService<IBotActionSelector>();
 
-                var requestResult = await MediatR.Send(callbackQueryResult.Value);
-
-                if (!requestResult.Succeeded && !requestResult.Errors.ContainsKey("not_found"))
+                try
                 {
-                    Logger.LogError("{CallbackQueryAction} request was not processed correctly: {@Errors}",
-                        callbackQueryResult.Value.GetType().Name, requestResult.Errors);
+                    var callbackActionResult = actionSelector.GetCallbackAction();
+
+                    if (!callbackActionResult.Succeeded) return;
+
+                    var requestResult = await MediatR.Send(callbackActionResult.Value);
+
+                    if (!requestResult.Succeeded && !requestResult.Errors.ContainsKey("not_found"))
+                    {
+                        Logger.LogError("{CallbackQueryAction} was not processed correctly: {@Errors}",
+                            callbackActionResult.Value.GetType().Name, requestResult.Errors);
+                    }
                 }
-            }
-            catch (Exception exception)
-            {
-                Logger.LogError(exception, "Unhandled error processing callback query ({@CallbackQuery}).", e.CallbackQuery);
-            }
-            finally
-            {
-                #pragma warning disable 4014
-                Bot.Client.AnswerCallbackQueryAsync(e.CallbackQuery.Id);
-                #pragma warning restore 4014
+                catch (Exception exception)
+                {
+                    Logger.LogError(exception, "Unhandled error processing callback query ({@CallbackQuery}).", e.CallbackQuery);
+                }
+                finally
+                {
+#pragma warning disable 4014
+                    Bot.Client.AnswerCallbackQueryAsync(e.CallbackQuery.Id);
+#pragma warning restore 4014
+                }
             }
         }
-        
+
         private async void HandleOnInlineQuery(object sender, InlineQueryEventArgs e)
         {
-            try
+            using (var scope = ServiceScopeFactory.CreateScope())
             {
-                var inlineQueryResult = BotActionFactory.GetInlineAction(e.InlineQuery);
-                
-                if (!inlineQueryResult.Succeeded) return;
+                var botContext = scope.ServiceProvider.GetRequiredService<IBotContext>();
+                botContext.Populate(e.InlineQuery);
 
-                var requestResult = await MediatR.Send(inlineQueryResult.Value);
+                var actionSelector = scope.ServiceProvider.GetRequiredService<IBotActionSelector>();
 
-                if (!requestResult.Succeeded && !requestResult.Errors.ContainsKey("not_found"))
+                try
                 {
-                    Logger.LogError("{InlineQueryAction} request was not processed correctly: {@Errors}",
-                        inlineQueryResult.Value.GetType().Name, requestResult.Errors);
+                    var inlineActionResult = actionSelector.GetInlineAction();
+
+                    if (!inlineActionResult.Succeeded) return;
+
+                    var requestResult = await MediatR.Send(inlineActionResult.Value);
+
+                    if (!requestResult.Succeeded && !requestResult.Errors.ContainsKey("not_found"))
+                    {
+                        Logger.LogError("{InlineQueryAction} request was not processed correctly: {@Errors}",
+                            inlineActionResult.Value.GetType().Name, requestResult.Errors);
+                    }
                 }
-            }
-            catch (Exception exception)
-            {
-                Logger.LogError(exception, "Unhandled error processing inline query ({@InlineQuery}).", e.InlineQuery);
+                catch (Exception exception)
+                {
+                    Logger.LogError(exception, "Unhandled error processing inline query ({@InlineQuery}).", e.InlineQuery);
+                }
             }
         }
     }
