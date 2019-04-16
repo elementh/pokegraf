f3bf0056311444b7f729fa3abee3a9c9dbce71ee
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-16 11:52:12 +0200

    Updated command actions: implemented CanHandle and added constructor

--- src/Pokegraf.Application.Implementation/BotActions/Commands/Fusion/FusionCommandAction.cs
+++ src/Pokegraf.Application.Implementation/BotActions/Commands/Fusion/FusionCommandAction.cs
@@ -1,3 +1,4 @@
+using Pokegraf.Application.Contract.Common.Context;
 using Pokegraf.Application.Implementation.BotActions.Common;
 using Pokegraf.Application.Implementation.Common.Actions;
 
@@ -5,6 +6,13 @@ namespace Pokegraf.Application.Implementation.BotActions.Commands.Fusion
 {
     public class FusionCommandAction : CommandAction
     {
-        
+        public FusionCommandAction(IBotContext botContext) : base(botContext)
+        {
+        }
+
+        public override bool CanHandle(string condition)
+        {
+            return condition == "/fusion";
+        }
     }
 }
--- src/Pokegraf.Application.Implementation/BotActions/Commands/Start/StartCommandAction.cs
+++ src/Pokegraf.Application.Implementation/BotActions/Commands/Start/StartCommandAction.cs
@@ -1,3 +1,4 @@
+using Pokegraf.Application.Contract.Common.Context;
 using Pokegraf.Application.Implementation.BotActions.Common;
 using Pokegraf.Application.Implementation.Common.Actions;
 
@@ -5,6 +6,13 @@ namespace Pokegraf.Application.Implementation.BotActions.Commands.Start
 {
     public class StartCommandAction : CommandAction
     {
-        
+        public StartCommandAction(IBotContext botContext) : base(botContext)
+        {
+        }
+
+        public override bool CanHandle(string condition)
+        {
+            return condition == "/start";
+        }
     }
 }
