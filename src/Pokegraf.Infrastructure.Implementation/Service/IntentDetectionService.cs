470a66aad67ce78337d151f3185944265c89ebf4
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-24 17:40:06 +0200

    Started with implementation of IntentDetectionService

---
+++ src/Pokegraf.Infrastructure.Implementation/Service/IntentDetectionService.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Dialogflow.V2;
+using Grpc.Auth;
+using Grpc.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Binder;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Pokegraf.Infrastructure.Contract.Dto;
+using Pokegraf.Infrastructure.Contract.Service;
+
+namespace Pokegraf.Infrastructure.Implementation.Service
+{
+    public class IntentDetectionService : IIntentDetectionService
+    {
+        protected readonly ILogger<IntentDetectionService> Logger;
+        protected readonly SessionsClient Client;
+        
+        public IntentDetectionService(ILogger<IntentDetectionService> logger, IConfiguration configuration)
+        {
+            Logger = logger;
+            
+            //TODO: refactor as custom config
+            var credentialsJson = JsonConvert.SerializeObject(configuration.GetSection("GoogleCredentials").Get<Dictionary<string, string>>());
+            
+            var credential = GoogleCredential.FromJson(credentialsJson);
+            
+            var channel = new Channel(SessionsClient.DefaultEndpoint.Host, SessionsClient.DefaultEndpoint.Port, credential.ToChannelCredentials());
+            
+            Client = SessionsClient.Create(channel);
+        }
+
+        public async Task<IntentDto> GetIntent(string userQuery)
+        {
+            var session = new SessionName("newagent-9bc74", Guid.NewGuid().ToString());
+            
+            var formalQuery = new QueryInput
+            {
+                Text = new TextInput
+                {
+                    Text = "I want info of pidgey?",
+                    LanguageCode = "en-us"
+                }
+            };
+
+            var response = await Client.DetectIntentAsync(session, formalQuery);
+            
+            return new IntentDto();
+        }
+    }
+}
