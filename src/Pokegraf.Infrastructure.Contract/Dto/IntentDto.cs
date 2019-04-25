ad2076a8e75e96cb01c06dc0032be08298ed7b8d
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-25 07:42:19 +0200

    Added attributes to IntentDto

--- src/Pokegraf.Infrastructure.Contract/Dto/IntentDto.cs
+++ src/Pokegraf.Infrastructure.Contract/Dto/IntentDto.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+
 namespace Pokegraf.Infrastructure.Contract.Dto
 {
     public class IntentDto
     {
-        
+        public string Name { get; set; }
+        public Dictionary<string, string> Parameters { get; set; }
+        public string FulfillmentText { get; set; }
+        public float IntentDetectionConfidence { get; set; }
     }
 }
