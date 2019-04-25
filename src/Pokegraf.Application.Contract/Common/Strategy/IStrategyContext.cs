c8ba20831cf2a3dcab201ec003bb586b2509c7df
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-25 13:33:16 +0200

    Updated StrategyContext to use ConversationActions too

--- src/Pokegraf.Application.Contract/Common/Strategy/IStrategyContext.cs
+++ src/Pokegraf.Application.Contract/Common/Strategy/IStrategyContext.cs
@@ -2,12 +2,14 @@ using System.Collections.Generic;
 using Pokegraf.Application.Contract.Model.Action;
 using Pokegraf.Application.Contract.Model.Action.Callback;
 using Pokegraf.Application.Contract.Model.Action.Command;
+using Pokegraf.Application.Contract.Model.Action.Conversation;
 using Pokegraf.Application.Contract.Model.Action.Inline;
 
 namespace Pokegraf.Application.Contract.Common.Strategy
 {
     public interface IStrategyContext
     {
+        IEnumerable<IConversationAction> GetConversationStrategyContext();
         IEnumerable<ICallbackAction> GetCallbackStrategyContext();
         IEnumerable<ICommandAction> GetCommandStrategyContext();
         IEnumerable<IInlineAction> GetInlineStrategyContext();
--- src/Pokegraf.Application.Implementation/Common/Strategy/StrategyContext.cs
+++ src/Pokegraf.Application.Implementation/Common/Strategy/StrategyContext.cs
@@ -3,23 +3,31 @@ using Pokegraf.Application.Contract.Common.Strategy;
 using Pokegraf.Application.Contract.Model.Action;
 using Pokegraf.Application.Contract.Model.Action.Callback;
 using Pokegraf.Application.Contract.Model.Action.Command;
+using Pokegraf.Application.Contract.Model.Action.Conversation;
 using Pokegraf.Application.Contract.Model.Action.Inline;
 
 namespace Pokegraf.Application.Implementation.Common.Strategy
 {
     public class StrategyContext : IStrategyContext
     {
+        protected IEnumerable<IConversationAction> ConversationActions;
         protected IEnumerable<ICallbackAction> CallbackActions;
         protected IEnumerable<ICommandAction> CommandActions;
         protected IEnumerable<IInlineAction> InlineActions;
 
-        public StrategyContext(IEnumerable<ICallbackAction> callbackActions, IEnumerable<ICommandAction> commandActions, IEnumerable<IInlineAction> inlineActions)
+        public StrategyContext(IEnumerable<IConversationAction> conversationActions, IEnumerable<ICallbackAction> callbackActions, IEnumerable<ICommandAction> commandActions, IEnumerable<IInlineAction> inlineActions)
         {
+            ConversationActions = conversationActions;
             CallbackActions = callbackActions;
             CommandActions = commandActions;
             InlineActions = inlineActions;
         }
 
+        public IEnumerable<IConversationAction> GetConversationStrategyContext()
+        {
+            return ConversationActions;
+        }
+        
         public IEnumerable<ICallbackAction> GetCallbackStrategyContext()
         {
             return CallbackActions;
