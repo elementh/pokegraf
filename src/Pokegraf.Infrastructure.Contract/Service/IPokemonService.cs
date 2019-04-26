42f42a2cdbdd2156000b625327e3a0c2faa597fd
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-26 15:18:40 +0200

    Added method to pokemon service interface

--- src/Pokegraf.Infrastructure.Contract/Service/IPokemonService.cs
+++ src/Pokegraf.Infrastructure.Contract/Service/IPokemonService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pokegraf.Common.Result;
 using Pokegraf.Infrastructure.Contract.Dto;
+using Pokegraf.Infrastructure.Contract.Dto.Pokemon;
 
 namespace Pokegraf.Infrastructure.Contract.Service
 {
@@ -11,5 +12,6 @@ namespace Pokegraf.Infrastructure.Contract.Service
         Task<Result<PokemonDto>> GetPokemon(int pokeNumber);
         Task<Result<PokemonDto>> GetPokemon(string pokeName);
         Result<PokemonFusionDto> GetFusion();
+        Task<Result<BerryDto>> GetBerry(string berryName);
     }
 }
