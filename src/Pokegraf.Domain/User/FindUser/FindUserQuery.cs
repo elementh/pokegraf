f39b8cd2edaac7829d48235ee49fcbedbb5c0c48
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-23 14:21:18 +0200

    Created FindUserQuery and it's handler

---
+++ src/Pokegraf.Domain/User/FindUser/FindUserQuery.cs
@@ -0,0 +1,10 @@
+﻿using Pokegraf.Common.Request;
+using Pokegraf.Common.Result;
+
+namespace Pokegraf.Domain.User.FindUser
+{
+    public class FindUserQuery : Request<Result<Entity.User>>
+    {
+        public int UserId { get; set; }
+    }
+}
