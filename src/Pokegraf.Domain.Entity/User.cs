da91d983dae94452dba6011afdef91b2c797be27
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-23 12:31:51 +0200

    Added Domain entities

---
+++ src/Pokegraf.Domain.Entity/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pokegraf.Domain.Entity
+{
+    public class User
+    {
+        /// <summary>
+        /// Id of the user.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Specifies if the user is a bot or no.
+        /// </summary>
+        public bool IsBot { get; set; }
+        /// <summary>
+        /// Language code of the user client.
+        /// </summary>
+        public string LanguageCode { get; set; }
+        /// <summary>
+        /// Username of the user.
+        /// </summary>
+        public string Username { get; set; }
+        /// <summary>
+        /// Date of first interaction with the user.
+        /// </summary>
+        public DateTime FirstSeen { get; set; }
+        /// <summary>
+        /// Custom name for the user.
+        /// </summary>
+        public string TrainerName { get; set; }
+        
+        public List<Conversation> Conversations { get; set; }
+    }
+}
