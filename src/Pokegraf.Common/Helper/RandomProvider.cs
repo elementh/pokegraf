f7ca01b076bef1bda54194108b26f7532e45c5d1
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-23 11:50:06 +0200

    Created a new RandomProvider

---
+++ src/Pokegraf.Common/Helper/RandomProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Pokegraf.Common.Helper
+{
+    /// <summary>
+    /// Random provider thanks to https://csharpindepth.com/Articles/Random
+    /// </summary>
+    public static class RandomProvider
+    {    
+        private static int seed = Environment.TickCount;
+
+        private static ThreadLocal<Random> randomWrapper = new ThreadLocal<Random>
+            (() => new Random(Interlocked.Increment(ref seed)));
+
+        public static Random GetThreadRandom()
+        {
+            return randomWrapper.Value;
+        }
+    }
+}
