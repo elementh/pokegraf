a9f8b74f8efcf4046f21badce14840def55ed24d
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-23 14:41:32 +0200

    Created UpdateAction

---
+++ src/Pokegraf.Application.Contract/Model/Action/Update/IUpdateAction.cs
@@ -0,0 +1,7 @@
+﻿﻿namespace Pokegraf.Application.Contract.Model.Action.Update
+{
+    public interface IUpdateAction : IBotAction
+    {
+        Telegram.Bot.Types.Update Update { get; set; }
+    }
+}
---
+++ src/Pokegraf.Application.Contract/Model/Action/Update/UpdateAction.cs
@@ -0,0 +1,14 @@
+﻿using Pokegraf.Application.Contract.Common.Context;
+
+namespace Pokegraf.Application.Contract.Model.Action.Update
+{
+    public abstract class UpdateAction : BotAction, IUpdateAction
+    {
+        public Telegram.Bot.Types.Update Update { get; set; }
+        public UpdateAction(IBotContext botContext) : base(botContext)
+        {
+            Update = botContext.Update;
+        }
+
+    }
+}
