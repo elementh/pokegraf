666ff6125038fc2d7d8f48fd99ad2322c3fb75d6
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-23 14:51:26 +0200

    Created UpdateUserTrainerName command and it's handler

---
+++ src/Pokegraf.Domain/User/UpdateUserTrainerName/UpdateUserTrainerNameCommand.cs
@@ -0,0 +1,17 @@
+﻿using Pokegraf.Common.Request;
+using Pokegraf.Common.Result;
+
+namespace Pokegraf.Domain.User.UpdateUserTrainerName
+{
+    public class UpdateUserTrainerNameCommand : Request<Result>
+    {
+        /// <summary>
+        /// Id of the user.
+        /// </summary>
+        public int UserId { get; set; }
+        /// <summary>
+        /// Trainer name of the user.
+        /// </summary>
+        public string TrainerName { get; set; }
+    }
+}
---
+++ src/Pokegraf.Domain/User/UpdateUserTrainerName/UpdateUserTrainerNameCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Pokegraf.Common.Request;
+using Pokegraf.Common.Result;
+using Pokegraf.Persistence.Contract;
+
+namespace Pokegraf.Domain.User.UpdateUserTrainerName
+{
+    public class UpdateUserTrainerNameCommandHandler : CommonHandler<UpdateUserTrainerNameCommand, Result>
+    {
+        protected readonly IUnitOfWork UnitOfWork;
+
+        public UpdateUserTrainerNameCommandHandler(ILogger<CommonHandler<UpdateUserTrainerNameCommand, Result>> logger, IMediator mediatR, IUnitOfWork unitOfWork) : base(logger, mediatR)
+        {
+            UnitOfWork = unitOfWork;
+        }
+
+        public override async Task<Result> Handle(UpdateUserTrainerNameCommand request, CancellationToken cancellationToken)
+        {
+            var user = await UnitOfWork.UserRepository.FindById(request.UserId);
+
+            user.TrainerName = request.TrainerName;
+            
+            try
+            {
+                await UnitOfWork.SaveAsync(cancellationToken);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, "Unhandled error updating user trainer name. UserId: {UserId}", user.Id);
+                
+                return Result.UnknownError(new List<string> {e.Message});
+            }
+            
+            return Result.Success();
+        }
+    }
+}
