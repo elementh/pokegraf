c7339bcf2913567d427787b2c30be9d18294e438
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-10-09 11:05:31 +0200

    Added ResultError

---
+++ src/Pokegraf.Common/ErrorHandling/ResultError.cs
@@ -0,0 +1,27 @@
+﻿﻿using System.Net;
+
+ namespace Pokegraf.Common.ErrorHandling
+{
+    public class ResultError
+    {
+        /// <summary>
+        /// Error type.
+        /// </summary>
+        public ResultErrorType Type { get; set; }
+        /// <summary>
+        /// Message describing the error.
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// Optional, available when error type is HttpError
+        /// </summary>
+        public HttpStatusCode Code { get; set; }
+    }
+
+    public enum ResultErrorType
+    {
+        NotFound,
+        HttpError,
+        UnknownError
+    }
+}
---
+++ src/Pokegraf.Common/ErrorHandling/ResultErrorHelper.cs
@@ -0,0 +1,35 @@
+﻿﻿using System.Net;
+
+ namespace Pokegraf.Common.ErrorHandling
+{
+    public static class ResultErrorHelper
+    {
+        public static ResultError NotFound(string message)
+        {
+            return new ResultError
+            {
+                Type = ResultErrorType.NotFound,
+                Message = message
+            };
+        }
+
+        public static ResultError HttpError(string message, HttpStatusCode statusCode)
+        {
+            return new ResultError
+            {
+                Type = ResultErrorType.HttpError,
+                Message = message,
+                Code = statusCode
+            };
+        }
+
+        public static ResultError UnknownError(string message)
+        {
+            return new ResultError
+            {
+                Type = ResultErrorType.UnknownError,
+                Message = message
+            };
+        }
+    }
+}
