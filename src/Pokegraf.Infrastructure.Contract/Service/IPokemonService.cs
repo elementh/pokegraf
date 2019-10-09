509bb91f3047a27acc2b01778a34cb465851a7b4
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-10-09 11:07:33 +0200

    Result handling rework

--- src/Pokegraf.Infrastructure.Contract/Service/IPokemonService.cs
+++ src/Pokegraf.Infrastructure.Contract/Service/IPokemonService.cs
@@ -1,17 +1,15 @@
-using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using Pokegraf.Common.Result;
-using Pokegraf.Infrastructure.Contract.Dto;
+using OperationResult;
+using Pokegraf.Common.ErrorHandling;
 using Pokegraf.Infrastructure.Contract.Dto.Pokemon;
 
 namespace Pokegraf.Infrastructure.Contract.Service
 {
     public interface IPokemonService
     {
-        Task<Result<PokemonDto>> GetPokemon(int pokeNumber);
-        Task<Result<PokemonDto>> GetPokemon(string pokeName);
-        Result<PokemonFusionDto> GetFusion();
-        Task<Result<BerryDto>> GetBerry(string berryName);
+        Task<Result<PokemonDto, ResultError>> GetPokemon(int pokeNumber);
+        Task<Result<PokemonDto, ResultError>> GetPokemon(string pokeName);
+        Result<PokemonFusionDto, ResultError> GetFusion();
+        Task<Result<BerryDto, ResultError>> GetBerry(string berryName);
     }
 }
