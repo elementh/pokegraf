aa0ca132b15689d469003e94f1feccce9f6c9f73
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-10-14 13:16:02 +0200

    Refactored TextResponseHandler

--- src/Pokegraf.Application.Implementation/Core/Responses/Text/TextResponseHandler.cs
+++ src/Pokegraf.Application.Implementation/Core/Responses/Text/TextResponseHandler.cs
@@ -4,32 +4,40 @@ using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using OperationResult;
 using Pokegraf.Application.Contract.Core.Context;
+using Pokegraf.Application.Contract.Core.Responses.Text;
+using Pokegraf.Common.ErrorHandling;
+using static OperationResult.Helpers;
+using static Pokegraf.Common.ErrorHandling.Helpers;
 
 namespace Pokegraf.Application.Implementation.Core.Responses.Text
 {
-    public class TextResponseHandler : Pokegraf.Common.Request.CommonHandler<TextResponse, Result>
+    public class TextResponseHandler : IRequestHandler<TextResponse, Status<ResultError>>
     {
+        protected readonly ILogger<TextResponseHandler> Logger;
         protected readonly IBotContext BotContext;
-
-        public TextResponseHandler(ILogger<Pokegraf.Common.Request.CommonHandler<TextResponse, Result>> logger, IMediator mediatR, IBotContext botContext) : base(logger, mediatR)
+        
+        public TextResponseHandler(ILogger<TextResponseHandler> logger, IBotContext botContext)
         {
+            Logger = logger;
             BotContext = botContext;
         }
 
-        public override async Task<Result> Handle(TextResponse request, CancellationToken cancellationToken)
+        public async Task<Status<ResultError>> Handle(TextResponse request, CancellationToken cancellationToken)
         {
             try
             {
                 await BotContext.BotClient.Client.SendTextMessageAsync(BotContext.Chat.Id, request.Text, request.ParseMode, cancellationToken: cancellationToken);
 
-                return Result.Success();
+                return Ok();
             }
             catch (Exception e)
             {
                 Logger.LogError(e, "Unhandled error sending text response ({@Request}).", request);
                 
-                return Result.UnknownError(new List<string> {e.Message});
+                return Error(UnknownError(e.Message));
+                
             }
         }
     }
