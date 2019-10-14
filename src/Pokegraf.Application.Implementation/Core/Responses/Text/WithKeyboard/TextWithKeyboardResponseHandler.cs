355ae5e0ee29460e2188bcbe6899da08991ddb5d
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-10-14 13:18:04 +0200

    Refactored TextWithKeyboardResponseHandler

---
+++ src/Pokegraf.Application.Implementation/Core/Responses/Text/WithKeyboard/TextWithKeyboardResponseHandler.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using OperationResult;
+using Pokegraf.Application.Contract.Core.Context;
+using Pokegraf.Application.Contract.Core.Responses.Text.WithKeyboard;
+using Pokegraf.Common.ErrorHandling;
+using static OperationResult.Helpers;
+using static Pokegraf.Common.ErrorHandling.Helpers;
+
+namespace Pokegraf.Application.Implementation.Core.Responses.Text.WithKeyboard
+{
+    public class TextWithKeyboardResponseHandler : IRequestHandler<TextWithKeyboardResponse, Status<ResultError>>
+    {
+        protected readonly ILogger<TextWithKeyboardResponseHandler> Logger;
+        protected readonly IBotContext BotContext;
+
+        public TextWithKeyboardResponseHandler(ILogger<TextWithKeyboardResponseHandler> logger, IBotContext botContext)
+        {
+            Logger = logger;
+            BotContext = botContext;
+        }
+
+        public async Task<Status<ResultError>> Handle(TextWithKeyboardResponse request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await BotContext.BotClient.Client.SendTextMessageAsync(BotContext.Chat.Id, request.Text, replyMarkup: request.Keyboard, cancellationToken: cancellationToken);
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, "Unhandled error sending text response with inline keyboard ({@Request}).", request);
+                
+                return Error(UnknownError(e.Message));
+            }
+        }
+    }
+}
