f9abf0c2d3cddddde46b22161a48898728a06873
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-10-14 09:35:25 +0200

    Created FindUserQuery and it's implementation

---
+++ src/Pokegraf.Domain/Core/User/Query/FindUser/FindUserQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MediatR;
+using OperationResult;
+using Pokegraf.Common.ErrorHandling;
+
+namespace Pokegraf.Domain.Core.User.Query.FindUser
+{
+    public class FindUserQuery : IRequest<Result<Entity.User, ResultError>>
+    {
+        public DateTime Timestamp { get; }
+
+        public FindUserQuery()
+        {
+            Timestamp = DateTime.UtcNow;
+        }
+        
+        public int UserId { get; set; }
+    }
+}
---
+++ src/Pokegraf.Domain/Core/User/Query/FindUser/FindUserQueryHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using OperationResult;
+using Pokegraf.Common.ErrorHandling;
+using Pokegraf.Persistence.Contract.Context;
+using static OperationResult.Helpers;
+using static Pokegraf.Common.ErrorHandling.ResultErrorHelper;
+
+namespace Pokegraf.Domain.Core.User.Query.FindUser
+{
+    public class FindUserQueryHandler : IRequestHandler<FindUserQuery, Result<Entity.User, ResultError>>
+    {
+        protected readonly ILogger<FindUserQueryHandler> Logger;
+        protected readonly IPokegrafDbContext Context;
+
+        public FindUserQueryHandler(ILogger<FindUserQueryHandler> logger, IPokegrafDbContext context)
+        {
+            Logger = logger;
+            Context = context;
+        }
+
+        public async Task<Result<Entity.User, ResultError>> Handle(FindUserQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var user = await Context.Users.AsNoTracking()
+                    .FirstOrDefaultAsync(u => u.Id == request.UserId, cancellationToken);
+
+                if (user == null)
+                {
+                    return Error(NotFound("user not found"));
+                }
+
+                return Ok(user);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, "Unhandled error finding user from DbContext");
+
+                return Error(UnknownError($"Unhandled error finding user from DbContext: {e.Message}."));
+            }
+        }
+    }
+}
