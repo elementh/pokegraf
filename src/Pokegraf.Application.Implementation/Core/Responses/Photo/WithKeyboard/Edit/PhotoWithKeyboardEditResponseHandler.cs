57bb3c5fe077923540401d5a5a9ba7171d9f87e5
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-10-14 14:09:19 +0200

    Refactored PhotoWithKeyboardResponse and Edit

---
+++ src/Pokegraf.Application.Implementation/Core/Responses/Photo/WithKeyboard/Edit/PhotoWithKeyboardEditResponseHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using OperationResult;
+using Pokegraf.Application.Contract.Core.Context;
+using Pokegraf.Application.Contract.Core.Responses.Photo.WithKeyboard.Edit;
+using Pokegraf.Common.ErrorHandling;
+using Telegram.Bot.Types;
+using static OperationResult.Helpers;
+using static Pokegraf.Common.ErrorHandling.Helpers;
+
+namespace Pokegraf.Application.Implementation.Core.Responses.Photo.WithKeyboard.Edit
+{
+    public class PhotoWithKeyboardEditResponseHandler : IRequestHandler<PhotoWithKeyboardEditResponse, Status<ResultError>>
+    {
+        protected readonly ILogger<PhotoWithKeyboardEditResponseHandler> Logger;
+        protected readonly IBotContext BotContext;
+        
+        public async Task<Status<ResultError>> Handle(PhotoWithKeyboardEditResponse request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var photo = new InputMediaPhoto(request.Photo);
+                
+                await BotContext.BotClient.Client.EditMessageMediaAsync(BotContext.Chat.Id, request.MessageId, photo, request.Keyboard, cancellationToken);
+
+                await BotContext.BotClient.Client.EditMessageCaptionAsync(BotContext.Chat.Id, request.MessageId, request.Caption, request.Keyboard, cancellationToken);
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, "Unhandled error updating photo with caption response with inline keyboard ({@Request}).", request);
+                
+                return Error(UnknownError(e.Message));
+            }
+        }
+    }
+}
---
+++ src/Pokegraf.Application.Implementation/Core/Responses/Photo/WithKeyboard/PhotoWithKeyboardResponseHandler.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using OperationResult;
+using Pokegraf.Application.Contract.Core.Context;
+using Pokegraf.Application.Contract.Core.Responses.Photo.WithKeyboard;
+using Pokegraf.Common.ErrorHandling;
+using static OperationResult.Helpers;
+using static Pokegraf.Common.ErrorHandling.Helpers;
+
+namespace Pokegraf.Application.Implementation.Core.Responses.Photo.WithKeyboard
+{
+    public class PhotoWithKeyboardResponseHandler : IRequestHandler<PhotoWithKeyboardResponse, Status<ResultError>>
+    {
+        protected readonly ILogger<PhotoWithKeyboardResponseHandler> Logger;
+        protected readonly IBotContext BotContext;
+        
+        public PhotoWithKeyboardResponseHandler(ILogger<PhotoWithKeyboardResponseHandler> logger, IBotContext botContext)
+        {
+            Logger = logger;
+            BotContext = botContext;
+        }
+
+        public async Task<Status<ResultError>> Handle(PhotoWithKeyboardResponse request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(request.Caption))
+                {
+                    await BotContext.BotClient.Client.SendPhotoAsync(BotContext.Chat.Id, request.Photo, replyMarkup: request.Keyboard, cancellationToken: cancellationToken);
+                }
+                else
+                {
+                    await BotContext.BotClient.Client.SendPhotoAsync(BotContext.Chat.Id, request.Photo, request.Caption, replyMarkup: request.Keyboard, cancellationToken: cancellationToken);
+                }
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, "Unhandled error sending photo response with inline keyboard ({@Request}).", request);
+
+                return Error(UnknownError(e.Message));
+            }
+        }
+    }
+}
