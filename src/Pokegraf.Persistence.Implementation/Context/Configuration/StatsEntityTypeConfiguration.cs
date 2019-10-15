59c4253bb0411019e8615a4c7e2315c1602b5d07
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-10-15 16:07:08 +0200

    Added StatsEntityTypeConfiguration

---
+++ src/Pokegraf.Persistence.Implementation/Context/Configuration/StatsEntityTypeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pokegraf.Domain.Entity;
+
+namespace Pokegraf.Persistence.Implementation.Context.Configuration
+{
+    public class StatsEntityTypeConfiguration : IEntityTypeConfiguration<Stats>
+    {
+        public void Configure(EntityTypeBuilder<Stats> builder)
+        {
+            builder.Property(e => e.Id).ValueGeneratedNever();
+
+            builder.OwnsOne(e => e.Requests);
+            
+            builder.HasOne(d => d.User)
+                .WithOne(p => p.Stats)
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}
