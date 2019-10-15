8a293ff79e03330a5c628f5453dd377cb9e3fd80
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-10-15 16:37:21 +0200

    Implemented AddOneToFusionRequestsCommand

---
+++ src/Pokegraf.Domain/Stats/Command/AddOneToFusionRequests/AddOneToFusionRequestsCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Pokegraf.Persistence.Contract;
+
+namespace Pokegraf.Domain.Stats.Command.AddOneToFusionRequests
+{
+    public class AddOneToFusionRequestsCommandHandler : IRequestHandler<AddOneToFusionRequestsCommand>
+    {
+        protected readonly ILogger<AddOneToFusionRequestsCommandHandler> Logger;
+        protected readonly IUnitOfWork UnitOfWork;
+
+        public AddOneToFusionRequestsCommandHandler(ILogger<AddOneToFusionRequestsCommandHandler> logger, IUnitOfWork unitOfWork)
+        {
+            Logger = logger;
+            UnitOfWork = unitOfWork;
+        }
+
+        public async Task<Unit> Handle(AddOneToFusionRequestsCommand request, CancellationToken cancellationToken)
+        {
+            var userStats = await UnitOfWork.StatsRepository.FindBy(st => st.UserId == request.UserId);
+
+            userStats.Requests.Fusion += 1;
+                
+            try
+            {
+                await UnitOfWork.SaveAsync(cancellationToken);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, "Unhandled error adding 1 to pokemon requests stats for user: @UserId", request.UserId);
+            }
+
+            return Unit.Value;
+        }
+    }
+}
