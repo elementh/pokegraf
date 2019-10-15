2ba1ae107ddcd3bf64bc3293efbf8d9552635f4d
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-10-15 14:56:37 +0200

    Updated start callback action

--- src/Pokegraf.Application.Implementation/Core/Actions/Callbacks/Start/StartCallbackActionHandler.cs
+++ src/Pokegraf.Application.Implementation/Core/Actions/Callbacks/Start/StartCallbackActionHandler.cs
@@ -7,6 +7,7 @@ using OperationResult;
 using Pokegraf.Application.Contract.Core.Responses.Text;
 using Pokegraf.Application.Contract.Core.Responses.Text.WithKeyboard.Edit;
 using Pokegraf.Common.ErrorHandling;
+using Pokegraf.Domain.User.Query.FindUser;
 using static OperationResult.Helpers;
 
 namespace Pokegraf.Application.Implementation.Core.Actions.Callbacks.Start
@@ -26,16 +27,23 @@ namespace Pokegraf.Application.Implementation.Core.Actions.Callbacks.Start
         {
             if (request.Data.ContainsKey("custom_name"))
             {
-                return await Mediator.Send(new TextWithKeyboardEditResponse(request.MessageId, null, "No problem Trainer! Use /setname `CUSTOM_NAME_HERE` to set a custom name!\n\nAnd when you are done try doing /pokemon"), cancellationToken);
+                return await Mediator.Send(new TextWithKeyboardEditResponse(request.MessageId, null, "No problem *Trainer*! Use /setname YOUR-NAME to set a custom name!\n\nAnd when you are done try doing /pokemon"), cancellationToken);
             }
 
             if (!request.Data.ContainsKey("requested_name")) return Ok();
 
-            await Mediator.Send(request.MapToUpdateUserTrainerNameCommand());
+            await Mediator.Send(request.MapToUpdateUserTrainerNameCommand(), cancellationToken);
 
-            return await Mediator.Send(
-                new TextWithKeyboardEditResponse(request.MessageId, null, $"Alright {request.Data.GetValueOrDefault("requested_name")}, why don't you try doing /pokemon ?"),
-                cancellationToken);
+            var user = await Mediator.Send(new FindUserQuery{ UserId = request.From.Id}, cancellationToken);
+
+            if (user.IsSuccess && user.Value?.TrainerName == request.Data.GetValueOrDefault("requested_name"))
+            {
+                return await Mediator.Send(
+                    new TextWithKeyboardEditResponse(request.MessageId, null, $"Alright *{user.Value.TrainerName}*, why don't you try doing /pokemon ?"),
+                    cancellationToken);
+            }
+            
+            return await Mediator.Send(new TextResponse("Oops, something went wrong *Trainer*! Try again /start"), cancellationToken);
         }
     }
 }
