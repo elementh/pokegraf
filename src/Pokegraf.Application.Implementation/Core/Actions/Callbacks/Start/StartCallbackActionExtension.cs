39f056637b4c6674d33b606d326a7d5fdd1b2760
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-10-15 14:36:10 +0200

    Implemented start callback action

---
+++ src/Pokegraf.Application.Implementation/Core/Actions/Callbacks/Start/StartCallbackActionExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Pokegraf.Domain.User.Command.UpdateUserTrainerName;
+
+namespace Pokegraf.Application.Implementation.Core.Actions.Callbacks.Start
+{
+    public static class StartCallbackActionExtension
+    {
+        public static UpdateUserTrainerNameCommand MapToUpdateUserTrainerNameCommand(this StartCallbackAction request)
+        {
+            return new UpdateUserTrainerNameCommand
+            {
+                UserId = request.From.Id,
+                TrainerName = request.Data.GetValueOrDefault("requested_name")
+            };
+        }
+    }
+}
---
+++ src/Pokegraf.Application.Implementation/Core/Actions/Callbacks/Start/StartCallbackActionHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using OperationResult;
+using Pokegraf.Application.Contract.Core.Responses.Text;
+using Pokegraf.Application.Contract.Core.Responses.Text.WithKeyboard.Edit;
+using Pokegraf.Common.ErrorHandling;
+using static OperationResult.Helpers;
+
+namespace Pokegraf.Application.Implementation.Core.Actions.Callbacks.Start
+{
+    public class StartCallbackActionHandler : IRequestHandler<StartCallbackAction, Status<Error>>
+    {
+        protected readonly ILogger<StartCallbackActionHandler> Logger;
+        protected readonly IMediator Mediator;
+
+        public StartCallbackActionHandler(ILogger<StartCallbackActionHandler> logger, IMediator mediator)
+        {
+            Logger = logger;
+            Mediator = mediator;
+        }
+
+        public async Task<Status<Error>> Handle(StartCallbackAction request, CancellationToken cancellationToken)
+        {
+            if (request.Data.ContainsKey("custom_name"))
+            {
+                return await Mediator.Send(new TextWithKeyboardEditResponse(request.MessageId, null, "No problem Trainer! Use /setname `CUSTOM_NAME_HERE` to set a custom name!\n\nAnd when you are done try doing /pokemon"), cancellationToken);
+            }
+
+            if (!request.Data.ContainsKey("requested_name")) return Ok();
+
+            await Mediator.Send(request.MapToUpdateUserTrainerNameCommand());
+
+            return await Mediator.Send(
+                new TextWithKeyboardEditResponse(request.MessageId, null, $"Alright {request.Data.GetValueOrDefault("requested_name")}, why don't you try doing /pokemon ?"),
+                cancellationToken);
+        }
+    }
+}
