5a565dd68c926fefbb75068183a8a1c0ef09ddf8
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-10-16 13:39:46 +0200

    Created GlobalStatsService interface

---
+++ src/Pokegraf.Application.Contract/Service/IGlobalStatsService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using OperationResult;
+using Pokegraf.Common.ErrorHandling;
+using Pokegraf.Domain.Entity;
+
+namespace Pokegraf.Application.Contract.Service
+{
+    /// <summary>
+    /// Optional middleware to get the global stats from a cached source.
+    /// </summary>
+    public interface IGlobalStatsService
+    {
+        Task<Result<Stats, Error>> Get();
+    }
+}
