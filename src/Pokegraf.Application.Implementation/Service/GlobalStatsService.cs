d60bf9a9a0916146260e1a393273991a86ab6036
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-10-16 14:19:37 +0200

    Implemented GlobalStatsService

---
+++ src/Pokegraf.Application.Implementation/Service/GlobalStatsService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using OperationResult;
+using Pokegraf.Application.Contract.Service;
+using Pokegraf.Common.ErrorHandling;
+using Pokegraf.Common.Helper;
+using Pokegraf.Domain.Stats.Model;
+using Pokegraf.Domain.Stats.Query.FindGlobalStats;
+
+namespace Pokegraf.Application.Implementation.Service
+{
+    public class GlobalStatsService : IGlobalStatsService
+    {
+        protected readonly ILogger<GlobalStatsService> Logger;
+        protected readonly IMediator Mediator;
+        protected readonly IMemoryCache Cache;
+
+        public GlobalStatsService(ILogger<GlobalStatsService> logger, IMediator mediator, IMemoryCache cache)
+        {
+            Logger = logger;
+            Mediator = mediator;
+            Cache = cache;
+        }
+
+        public async Task<Result<GlobalStats, Error>> Get(CancellationToken cancellationToken = default)
+        {
+            GlobalStats cacheEntry;
+            
+            if (!Cache.TryGetValue(CacheKeys.GlobalStats, out cacheEntry))
+            {
+                // Key not in cache, so get data.
+                var result = await Mediator.Send(new FindGlobalStatsQuery(), cancellationToken);
+
+                // Set cache options.
+                var cacheEntryOptions = new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromHours(1));
+
+                // Save data in cache.
+                Cache.Set(CacheKeys.GlobalStats, cacheEntry, cacheEntryOptions);
+            }
+
+            return cacheEntry;
+        }
+    }
+}
