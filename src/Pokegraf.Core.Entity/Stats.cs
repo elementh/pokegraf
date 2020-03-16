1dc4801ce47d072f6b849a4911c44a24c375ea0d
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-16 14:54:18 +0100

    Moved entities to core

---
+++ src/Pokegraf.Core.Entity/Stats.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Pokegraf.Core.Entity
+{
+    public class Stats
+    {
+        /// <summary>
+        /// Id of the stats.
+        /// </summary>
+        public Guid Id { get; set; }
+        /// <summary>
+        /// Id of the trainer.
+        /// </summary>
+        public int TrainerId { get; set; }
+        /// <summary>
+        /// Request related stats.
+        /// </summary>
+        public RequestStats Requests { get; set; }
+        /// <summary>
+        /// Trainer
+        /// </summary>
+        public Trainer Trainer { get; set; }
+
+        public class RequestStats
+        {
+            /// <summary>
+            /// Id of the request stats.
+            /// </summary>
+            public Guid Id { get; set; }
+            /// <summary>
+            /// Number of times a pokemon request has been made.
+            /// </summary>
+            public int Pokemon { get; set; }
+            /// <summary>
+            /// Number of times a fusion request has been made.
+            /// </summary>
+            public int Fusion { get; set; }
+        }
+    }
+}
---
+++ src/Pokegraf.Core.Entity/Trainer.cs
@@ -0,0 +1,12 @@
+﻿using Navigator.Extensions.Store.Entity;
+
+namespace Pokegraf.Core.Entity
+{
+    public class Trainer : User
+    {
+        /// <summary>
+        /// Custom name for the user.
+        /// </summary>
+        public string TrainerName { get; set; }
+    }
+}
