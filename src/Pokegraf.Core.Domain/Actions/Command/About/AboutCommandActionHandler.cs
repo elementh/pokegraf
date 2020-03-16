d5947b475b5a2674ebc38fa2ab0819b1b2953085
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-16 17:49:36 +0100

    Renamed resources file

--- src/Pokegraf.Core.Domain/Actions/Command/About/AboutCommandActionHandler.cs
+++ src/Pokegraf.Core.Domain/Actions/Command/About/AboutCommandActionHandler.cs
@@ -17,7 +17,7 @@ namespace Pokegraf.Core.Domain.Actions.Command.About
         public override async Task<Unit> Handle(AboutCommandAction request, CancellationToken cancellationToken)
         {
             await Ctx.Client.SendTextMessageAsync(Ctx.GetTelegramChat().Id, 
-                PokegrafDomainResources.AboutText, 
+                PokegrafCoreDomainResources.AboutText, 
                 ParseMode.Markdown,
                 cancellationToken: cancellationToken);
         
--- src/Pokegraf.Core.Domain/PokegrafCoreDomainResources.Designer.cs
+++ src/Pokegraf.Core.Domain/PokegrafCoreDomainResources.Designer.cs
@@ -22,14 +22,14 @@ namespace Pokegraf.Core.Domain {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class PokegrafDomainResources {
+    public class PokegrafCoreDomainResources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal PokegrafDomainResources() {
+        internal PokegrafCoreDomainResources() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Pokegraf.Core.Domain {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Pokegraf.Core.Domain.PokegrafDomainResources", typeof(PokegrafDomainResources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Pokegraf.Core.Domain.PokegrafCoreDomainResources", typeof(PokegrafCoreDomainResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
