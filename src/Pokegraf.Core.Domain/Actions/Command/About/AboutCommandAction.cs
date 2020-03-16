ea6e6fd2211c708025dbcececc700f675295f78d
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-16 17:49:00 +0100

    Refactored first action, about command

---
+++ src/Pokegraf.Core.Domain/Actions/Command/About/AboutCommandAction.cs
@@ -0,0 +1,13 @@
+﻿using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace Pokegraf.Core.Domain.Actions.Command.About
+{
+    public class AboutCommandAction : CommandAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return Command == "/about";
+        }
+    }
+}
---
+++ src/Pokegraf.Core.Domain/Actions/Command/About/AboutCommandActionHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Actions;
+using Telegram.Bot.Types.Enums;
+
+namespace Pokegraf.Core.Domain.Actions.Command.About
+{
+    public class AboutCommandActionHandler : ActionHandler<AboutCommandAction>
+    {
+        public AboutCommandActionHandler(INavigatorContext ctx) : base(ctx)
+        {
+        }
+
+        public override async Task<Unit> Handle(AboutCommandAction request, CancellationToken cancellationToken)
+        {
+            await Ctx.Client.SendTextMessageAsync(Ctx.GetTelegramChat().Id, 
+                PokegrafDomainResources.AboutText, 
+                ParseMode.Markdown,
+                cancellationToken: cancellationToken);
+        
+            return default;
+        }
+    }
+}
